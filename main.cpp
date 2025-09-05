d63cd491e6adf1882d661e424f4d093b5e455ef0
KaungMinThu256 <kaungminthu1256@gmail.com>
2025-09-05 15:18:50 +0000

    Calculate Rectangle Area

--- main.cpp
+++ main.cpp
@@ -2,8 +2,18 @@
 using namespace std;
 
 int main() {
-  const double length = 7.2;
-  const double width = 1.3;
+  
+    double length = 7.2;
+    double width = 1.3;
 
+    double area = length * width;
+    double perimeter = 2 * (length + width);
 
+    cout << "Rectangle Properties:" << endl;
+    cout << "Length = " << length << endl;
+    cout << "Width = " << width << endl;
+    cout << "Area = " << area << endl;
+    cout << "Perimeter = " << perimeter << endl;
+
+    return 0;
 }
